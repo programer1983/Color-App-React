93a4b2eb9b4878ef3a8f8f9ea03ce9f710b9d5e5
programer1983 <programer972@gmail.com>
2022-05-21 13:20:38 +0300

    creating a square component and input, props;

---
+++ src/Input.js
@@ -0,0 +1,18 @@
+
+const Input = ({colorValue, setColorValue}) => {
+  return (
+    <form onSubmit={(e) => e.preventDefault()}>
+      <label>Add Color Name:</label>
+      <input
+         autoFocus
+         type="text"
+         placeholder="Add Color Name"
+         required
+         value={colorValue}
+         onChange={(e) => setColorValue(e.target.value)}
+    />
+    </form>
+  )
+}
+
+export default Input
